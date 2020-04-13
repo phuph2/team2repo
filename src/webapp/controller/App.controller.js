9195df67b96b5f5fabbb6a70add040f5c6f8f172
phuph2 <phupham0310@gmail.com>
2020-04-13 07:05:52 +0000

    commit structure code

---
+++ src/webapp/controller/App.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
+	"use strict";
+	
+	return Controller.extend("Team2.controller.App", {
+		onInit : function () {
+	         // set data model on view
+	         var oData = {
+	            helloObject : {
+	               value : "Hello Team 2 !"
+	            }
+	         };
+	         var oModel = new JSONModel(oData);
+	         this.getView().setModel(oModel);
+    	}
+			
+	});
+});
