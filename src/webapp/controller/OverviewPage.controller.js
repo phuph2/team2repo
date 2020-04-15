c0363aaa8d4500534ce18a7b058d828bbdf77089
Tâm Nguyễn Thanh <captaindkid0105@gmail.com>
2020-04-15 08:59:25 +0000

    Create overview page(xml + controller).

--- src/webapp/controller/OverviewPage.controller.js
+++ src/webapp/controller/OverviewPage.controller.js
@@ -6,11 +6,10 @@ sap.ui.define([
 	
 	return Controller.extend("Team2.controller.OverviewPage", {
 		onInit: function () {
-			var oMaterialsModel = new JSONModel(sap.ui.require.toUrl("Team2/localService/mockdata/Materials.json")),
-				oStocksModel =  new JSONModel(sap.ui.require.toUrl("Team2/localService/mockdata/Stocks.json"));
-
-			this.getView().setModel(oMaterialsModel, "materials");
-			this.getView().setModel(oStocksModel, "stocks");
+		
+		},
+		onNavTo: function(oEvent){
+			
 		}
 			
 	});
